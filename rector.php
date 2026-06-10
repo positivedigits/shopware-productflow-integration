aa508c20bc6c630e905e9a573d57783270166223
dfsoeten <19746471+dfsoeten@users.noreply.github.com>
2026-06-10 10:59:37 +0200

    chore: added rector

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        naming: false,
+    )
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+;
